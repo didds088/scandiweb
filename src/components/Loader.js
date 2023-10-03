f2c966abb76d9805f0b5790f93f128b67319f58b
Agusiobo Kingsley <Agusiobokingsley@outlook.com>
2023-10-03 14:03:11 +0100

    10/03/23

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,7 @@
 const Loader = () => { 
     return (
     <div className="text-center">
-    <div className="spinner-border text-danger" role="status">
+    <div className="spinner-border text-secondary" role="status">
     <span className="visually-hidden">Loading...</span>
     </div>
     </div>
